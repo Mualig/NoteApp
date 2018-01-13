b57ffc128c8268391ee5ed0802d55b840c0c0792
Guillaume Gaborel <mualig@gmail.com>
2018-01-13 13:47:10 +0100

    Chap 3/14. Ajout de yargs. Ajout de fonctions dans notes.

--- notes.js
+++ notes.js
@@ -1,6 +1,25 @@
 console.log('Starting notes');
 
-module.exports.addNote = () => {
-    console.log('add note !');
+let addNote = (title, body) => {
+    console.log('Adding note: ', title, body);
     return 'new Note';
+};
+
+let getAll = () => {
+  console.log('Getting all notes');
+};
+
+let getNote = (title) => {
+    console.log('Getting note ', title);
+};
+
+let removeNote = (title) => {
+    console.log('Removing note ', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
