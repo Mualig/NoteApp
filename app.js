f5c3e67f7797aeb05e688495488f35bdf541e062
Guillaume Gaborel <mualig@gmail.com>
2018-01-13 12:24:17 +0100

    Cleanup

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 console.log('Starting App !');
 
 const fs = require('fs');
-const os = require('os');
-
-const notes = require('./notes.js');
 const _ = require('lodash');
 
-let filterArray = _.uniq(['Bob', 1, 2, false, 'bob', 'Bob']);
-console.log(filterArray);
-
-// let user = os.userInfo();
-//
-// fs.appendFile('./greetings.txt', `Hello World ${user.username}, you are ${notes.age}\n`, function (error) {
-//     if (error) {
-//         console.log('Error', error);
-//     }
-// });
-
-let result = notes.add(1, 3);
-console.log(result);
+const notes = require('./notes.js');
--- notes.js
+++ notes.js
@@ -4,7 +4,3 @@ module.exports.addNote = () => {
     console.log('add note !');
     return 'new Note';
 };
-
-module.exports.add = (a, b) => {
-    return a + b;
-};
